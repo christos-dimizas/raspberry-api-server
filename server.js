491818ea53a355cfd07c5df22f37f6691ba2fe31
christos.dimizas <christos.dimizas@marinetraffic.com>
2019-03-24 10:38:15 +0200

    Add buttonLed example

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const buttonLed = require('./raspiExamples/buttonLed.js');
 const app = express();
 
 // reply to request with "Hello World!"
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/raspi/buttonLed', (req, res) => {
+  buttonLed();
 });
 
 //start a server on port 80 and log its start to our console
