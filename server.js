4e9f2f7aaf4e5b3b612ab50777c3327275475ef0
christos.dimizas <christos.dimizas@marinetraffic.com>
2019-03-24 11:26:42 +0200

    change port

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/raspi/buttonLed', (req, res) => {
 });
 
 //start a server on port 80 and log its start to our console
-const server = app.listen(80, function () {
+const server = app.listen(8080, function () {
 
   const port = server.address().port;
   console.log('Example app listening on port ', port);
